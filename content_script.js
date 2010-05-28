4b2c5860858e84b70f5ef96d38f8f546a79e0f98
Gordon Mei <gordeon@gmail.com>
2010-05-28 12:47:18 -0700

    formatting and manifest updates

--- content_script.js
+++ content_script.js
@@ -1,15 +1,29 @@
-var allentrycontent = "";
-var entrycontent = document.getElementsByClassName("entry-content");
-for (var i=0; i<entrycontent.length; i++) {
-	allentrycontent += entrycontent[i].innerHTML;
+if (document.getElementsByClassName("entry-content").length == 0) {
+	codeslist = '<li id="placeholder">No promo codes found.</li>';
 }
-var codeslist = "";
+else {
+	
+	var allentrycontent = "";	
 
-//var allcodes = allentrycontent.match(/(^|\s+)[A-Za-z0-9]{12}/g);
-var allcodes = allentrycontent.match(/[A-Z0-9]{12}/g);
+	var entrycontent = document.getElementsByClassName("entry-content");
 
-for (var j=0; j<allcodes.length; j++) {
-	codeslist += '<li><a href="https://phobos.apple.com/WebObjects/MZFinance.woa/wa/freeProductCodeWizard?code=' + allcodes[j] + '" style="cursor:pointer">' + allcodes[j] + '</a></li>';
+	for (var i=0; i<entrycontent.length; i++) {
+		allentrycontent += entrycontent[i].innerHTML;
+	}
+	var codeslist = "";
+
+	//var allcodes = allentrycontent.match(/(^|\s+)[A-Za-z0-9]{12}/g);
+	var allcodes = allentrycontent.match(/[A-Z0-9]{12}/g);
+
+	for (var j=0; j<allcodes.length; j++) {
+		codeslist += '<li><a href="https://phobos.apple.com/WebObjects/MZFinance.woa/wa/freeProductCodeWizard?code=' + allcodes[j] + '" target="blank">' + allcodes[j] + '</a></li>';
+	}
+	codeslist += '<li id="note">Click any of these to directly redeem in iTunes.</li>';
+
+	if (allcodes.length == 0) {
+		codeslist = '<li id="placeholder">No promo codes found.</li>';
+	}
+
 }
 
 chrome.extension.sendRequest({message: codeslist}, function(response) {});
